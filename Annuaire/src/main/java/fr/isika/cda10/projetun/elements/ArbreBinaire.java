af22d0b133fce95eb2746912be882f4c948b9fb9
Patrick Biedron <Patrick Biedron@DESKTOP-7JCOKNI>
2021-05-14 10:12:25 +0200

    premier commit du projet

---
+++ Annuaire/src/main/java/fr/isika/cda10/projetun/elements/ArbreBinaire.java
@@ -0,0 +1,8 @@
+package fr.isika.cda10.projetun.elements;
+
+public class ArbreBinaire {
+
+	public static void main(String[] args) {
+		System.out.println("juste pour que le contenu ne soit pas vide");
+	}
+}
